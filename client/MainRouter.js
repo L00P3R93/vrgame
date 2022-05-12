c31a2ef56b44c91bdc1d12de7d10ebb260c2236e
L00P3R93 <vincentkioko51@gmail.com>
2022-05-12 15:12:50 +0300

    Update II

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -8,19 +8,24 @@ import EditProfile from './user/EditProfile'
 import Profile from './user/Profile'
 import PrivateRoute from './auth/PrivateRoute'
 import Menu from './core/Menu'
+import NewGame from './game/NewGame'
+import EditGame from './game/EditGame'
 
 const MainRouter = () => {
-    return (<div>
-      <Menu/>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/users" component={Users}/>
-        <Route path="/signup" component={Signup}/>
-        <Route path="/signin" component={Signin}/>
-        <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
-        <Route path="/user/:userId" component={Profile}/>
-      </Switch>
-    </div>)
+		return (<div>
+			<Menu/>
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route path="/users" component={Users}/>
+				<Route path="/signup" component={Signup}/>
+				<Route path="/signin" component={Signin}/>
+				<PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
+				<Route path="/user/:userId" component={Profile}/>
+
+				<PrivateRoute path="/game/new" component={NewGame}/>
+        		<PrivateRoute path="/game/edit/:gameId" component={EditGame}/>
+			</Switch>
+		</div>)
 }
 
 export default MainRouter
